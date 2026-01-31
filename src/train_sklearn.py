69655a7fdd0d57583b3b11db40f3a4de985e0279
Bradley Pattengale <pattengalebradley@gmail.com>
2026-01-31 23:43:47 +0000

    Fix function call

--- src/train_sklearn.py
+++ src/train_sklearn.py
@@ -7,15 +7,6 @@ from sklearn.metrics import mean_squared_error
 from preprocess import run_preprocessing
 from load_data import load_train_data
 
-def train_lm(x_train, y_train):
-       lm = LinearRegression()
-       lm.fit(x_train, y_train)
-       return lm
-
-def train_rf(x_train, y_train):
-       rf = RandomForestRegressor(n_estimators=300, max_depth=20, min_samples_split=5, random_state=5)
-       rf.fit(x_train, y_train)
-       return rf
 
 def train_models():
 
@@ -25,7 +16,8 @@ def train_models():
        x_train, x_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=5)
 
        # training basic linear model
-       linear_model = train_lm(x_train, y_train)
+       linear_model = LinearRegression()
+       linear_model.fit(x_train, y_train)
 
        predict_lm = linear_model.predict(x_test)
        rmse_linear_model = np.sqrt(mean_squared_error(y_test, predict_lm))
@@ -36,7 +28,8 @@ def train_models():
        print('Linear Regressor RMSE: ', rmse_linear_model)
 
        # training random forrest
-       forest = train_rf(x_train, y_train)
+       forest = RandomForestRegressor(n_estimators=300, max_depth=20, min_samples_split=5, random_state=5)
+       forest.fit(x_train, y_train)
 
        predict_forest = forest.predict(x_test)
        rmse_forest = np.sqrt(mean_squared_error(y_test, predict_forest))
